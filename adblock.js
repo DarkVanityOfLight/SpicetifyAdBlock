954ebe8bd27b1dd95b05e84a40e95a26a91f7385
--global <darkvanity.oflight@gmail.com>
2022-02-20 14:08:48 +0100

    feat: Block htop

--- adblock.js
+++ adblock.js
@@ -9,7 +9,7 @@
 
     styleSheet.innerHTML =
      `
-   .MnW5SczTcbdFHxLZ_Z8j, .WiPggcPDzbwGxoxwLWFf, .ReyA3uE3K7oEz7PTTnAn, .main-leaderboardComponent-container, .sponsor-container, a.link-subtle.main-navBar-navBarLink.GKnnhbExo0U9l7Jz2rdc{
+   .hx2D0g_ursHte5bm62U2, .MnW5SczTcbdFHxLZ_Z8j, .WiPggcPDzbwGxoxwLWFf, .ReyA3uE3K7oEz7PTTnAn, .main-leaderboardComponent-container, .sponsor-container, a.link-subtle.main-navBar-navBarLink.GKnnhbExo0U9l7Jz2rdc{
     display: none !important;
     }
     `
